b71bf5571f9b1f844a7ab7cfdce6e0d24f91500d
gustavo-palabral-tierconnect-com <gustavo.palabral@tierconnect.com>
2022-05-25 16:26:46 -0300

    Creating repo

---
+++ src/test/java/BuscarGoogleTest.java
@@ -0,0 +1,88 @@
+import java.util.concurrent.TimeUnit;
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import java.util.concurrent.TimeUnit;
+import io.github.bonigarcia.wdm.*;
+
+/****************************************/
+// Historia de Usuario: Como usuario quiero buscar terminos en Google
+//
+// Prueba de Aceptacion: Verificar que el criterio de busqueda sea mostrado en los resultados
+/****************************************/
+
+public class BuscarGoogleTest {
+    
+    private WebDriver driver;
+    
+    @BeforeTest
+    public void setDriver() throws Exception{
+        //geckodriver
+        String path = "/Users/gustavo/Downloads/chromedriver102";
+        
+        System.setProperty("webdriver.chrome.driver", path);
+        //System.setProperty("webdriver.gecko.driver", path);
+        
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+        
+        //WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+        /*driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        */
+    }
+    
+    @Test
+    public void paginaPrincipalGoogle(){
+        
+        //1. Preparacion de la prueba
+        String googleUrl = "https://www.google.com";
+        driver.get(googleUrl);
+        
+        //2. Logica de la prueba
+        
+        //1. Capturar el campo de busqueda
+        WebElement campoBusqueda = driver.findElement(By.name("q"));
+        
+        //2. Escribir el criterio de busqueda
+        campoBusqueda.sendKeys("UCB");
+        
+        
+        //3. Mandar peticion
+        campoBusqueda.submit();
+        
+        
+        try{
+            TimeUnit.SECONDS.sleep(5);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        //3. Verificacion de la situacion esperada - Assert
+        WebElement resultado = driver.findElement(By.xpath("/html/body/div[7]/div/div[10]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/h2/span"));
+        
+        
+        String label = resultado.getText();
+        
+        Assert.assertEquals("Universidad Católica Boliviana \"San Pablo\" Regional La Paz", label);
+        
+    }
+    
+    @AfterTest
+    public void closeDriver() throws Exception{
+        driver.quit();
+    }
+    
+}
---
+++ src/test/java/RegistroDZoneTest.java
@@ -0,0 +1,87 @@
+import java.util.concurrent.TimeUnit;
+import junit.framework.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class RegistroDZoneTest {
+    
+    private WebDriver driver;
+    
+    //@BeforeTest
+    public void setDriver() throws Exception{
+        
+        String path = "/Users/gustavo/Downloads/chromedriver95";
+        
+        System.setProperty("webdriver.chrome.driver", path);
+        driver = new ChromeDriver();
+        
+    }
+    
+    //@Test
+    public void verificarMensajeErrorAlRegistrar(){
+        
+        //Preparacion de la prueba
+        String dzoneUrl = "https://dzone.com";
+        driver.get(dzoneUrl);
+        
+        //Logica de la prueba
+        WebElement joinLink = driver.findElement(By.xpath("//*[@id=\"ng-app\"]/body/div[2]/div/div/div[1]/div/div[2]/div[2]/a[2]"));
+        String linkText = joinLink.getText();
+        
+        System.out.println("Texto del Link:: "+linkText);
+        
+        joinLink.click();
+        
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        //Hacer click en boton JOIN
+        
+        WebElement joinButton = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div[2]/div[3]/button"));
+        
+        joinButton.click();
+        
+        System.out.println("Button Text:: "+joinButton.getText());
+        
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        //Verificacion - ASSERT
+        
+        WebElement iconAlert = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div[2]/div[1]/div[2]/form/div[1]/span[2]/i"));
+        Assert.assertEquals(true, iconAlert.isDisplayed());
+        
+        System.out.println("Se muestra el icono? "+iconAlert.isDisplayed());
+        
+        WebElement emailErrorMessage = driver.findElement(By.xpath("//div[@data-validate=\"Please enter a valid email address\"]"));
+        String attribute = emailErrorMessage.getAttribute("data-validate");
+        System.out.println("attribute::: "+attribute);
+        
+        Assert.assertEquals("Please enter a valid email address", attribute);
+        
+    }
+    
+    
+    
+    //@AfterTest
+    public void closeDriver() throws Exception{
+        driver.quit();
+    }
+    
+    
+    
+}
