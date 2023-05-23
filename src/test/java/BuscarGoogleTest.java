c059994a985e442eab17339f440e621e975c8860
gustavo-palabral-tierconnect-com <gustavo.palabral@tierconnect.com>
2023-05-22 20:19:23 -0400

    modificacion a pruebas

--- src/test/java/BuscarGoogleTest.java
+++ src/test/java/BuscarGoogleTest.java
@@ -1,5 +1,4 @@
 import java.util.concurrent.TimeUnit;
-import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,11 +11,18 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import java.util.concurrent.TimeUnit;
 import io.github.bonigarcia.wdm.*;
+import org.testng.Assert;
 
 /****************************************/
 // Historia de Usuario: Como usuario quiero buscar terminos en Google
 //
 // Prueba de Aceptacion: Verificar que el criterio de busqueda sea mostrado en los resultados
+//
+// 1. Ingresar a la pagina de Google
+// 2. En el campo de texto, escribir un criterio de busqueda i.e. Universidad Catolica Boliviana
+// 3. Presionar la tecla Enter
+// 
+// Resultado Esperado: El criterio de busqueda, i.e. Universidad Catolica Boliviana, debe mostrarse en el resultado
 /****************************************/
 
 public class BuscarGoogleTest {
@@ -25,66 +31,87 @@ public class BuscarGoogleTest {
     
     @BeforeTest
     public void setDriver() throws Exception{
-        //geckodriver
         String path = "/Users/gustavo/Downloads/chromedriver102";
         
         System.setProperty("webdriver.chrome.driver", path);
-        //System.setProperty("webdriver.gecko.driver", path);
         
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-
-        //WebDriverManager.firefoxdriver().setup();
-        //driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
-        
-        /*driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        */
     }
     
     @Test
     public void paginaPrincipalGoogle(){
         
-        //1. Preparacion de la prueba
+        /**************Preparacion de la prueba***********/
+    	
+    	//1. Ingresar a la pagina de Google
         String googleUrl = "https://www.google.com";
         driver.get(googleUrl);
         
-        //2. Logica de la prueba
+        /**************Logica de la prueba***************/
+        // 2. En el campo de texto, escribir un criterio de busqueda
         
-        //1. Capturar el campo de busqueda
+        /*Capturar el campo de busqueda*/
         WebElement campoBusqueda = driver.findElement(By.name("q"));
-        
-        //2. Escribir el criterio de busqueda
+
         campoBusqueda.sendKeys("Universidad Catolica Boliviana");
         
-        
-        //3. Mandar peticion
-        campoBusqueda.submit();
-        
-        
         try{
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(3);
         }
         catch(InterruptedException e){
             e.printStackTrace();
         }
         
-        //3. Verificacion de la situacion esperada - Assert
-        WebElement resultado = driver.findElement(By.xpath("/html/body/div[7]/div/div[10]/div[2]/div/div/div[2]/div/div[1]/div[2]/div[1]/div/div[2]/h2/span"));
+        //3. Presionar la tecla Enter
+        campoBusqueda.submit();
         
+        try{
+            TimeUnit.SECONDS.sleep(7);
+        }
+        catch(InterruptedException e){
+        	e.printStackTrace();
+        }    
         
+        /************Verificacion de la situacion esperada - Assert***************/
+        WebElement resultado = driver.findElement(By.xpath("//*[@id=\"rso\"]/div[1]/div/div/div/table/tbody/tr[1]/td/div/h3/a"));
+
+
         String label = resultado.getText();
         System.out.println("Texto del resultado:: "+label);
         
-        Assert.assertEquals("Universidade Católica Boliviana San Pablo (Universidad Católica Boliviana \"San Pablo\" Regional La Paz)", label);
+        
+        Assert.assertEquals(label,"Universidad Católica Boliviana");
         
     }
     
+   
     @AfterTest
     public void closeDriver() throws Exception{
         driver.quit();
     }
     
+    
+    
+    
+    /*
+     
+        
+        
+
+        
+        
+        
+        
+       
+
+      */
+    
 }
+
+
+
+
+
+
+
--- src/test/java/RegistroDZoneTest.java
+++ src/test/java/RegistroDZoneTest.java
@@ -11,6 +11,19 @@ import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+/****************************************/
+//Historia de Usuario: Como usuario nuevo quiero registrar mis datos
+//
+//Prueba de Aceptacion: Verificar que se muestren alertas para los campos obligatorios
+//
+//1. Ingresar a la pagina de DZone
+//2. Hacer en el link Join
+//3. Presionar el boton Join
+//
+//Resultado Esperado: Se deben mostrar mensajes de alerta para los campos obligatorios que no fueron llenados
+/****************************************/
+
+
 public class RegistroDZoneTest {
     
     private WebDriver driver;
@@ -30,34 +43,38 @@ public class RegistroDZoneTest {
     @Test
     public void verificarMensajeErrorAlRegistrar(){
         
-        //Preparacion de la prueba
+        /********** Preparacion de la prueba **********/
+    	
+    	//1. Ingresar a la pagina de DZone
         String dzoneUrl = "https://dzone.com";
         driver.get(dzoneUrl);
         
-        //Logica de la prueba
+        /*********** Logica de la prueba***********/
         
-        WebElement joinLink = driver.findElement(By.xpath("//*[@id=\"ng-app\"]/body/div[2]/div/div/div[1]/div/div[2]/div[2]/a[2]"));
+        //2. Hacer en el link Join
+        
+        WebElement joinLink = driver.findElement(By.xpath("//*[@id=\"unauthenticated-block\"]/div[2]/a[2]"));
         String linkText = joinLink.getText();
         
         System.out.println("Texto del Link:: "+linkText);
         
         joinLink.click();
         
+        //Esperamos 3 segundos
         try{
             TimeUnit.SECONDS.sleep(3);
         }
         catch(InterruptedException e){
             e.printStackTrace();
+     
         }
         
-        //Hacer click en boton JOIN
+        //3. Presionar el boton Join
         
         WebElement joinButton = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div[2]/div[3]/button"));
         
         joinButton.click();
         
-        System.out.println("Button Text:: "+joinButton.getText());
-        
         try{
             TimeUnit.SECONDS.sleep(3);
         }
@@ -65,7 +82,7 @@ public class RegistroDZoneTest {
             e.printStackTrace();
         }
         
-        //Verificacion - ASSERT
+        /************Verificacion de la situacion esperada - Assert***************/
         
         WebElement iconAlert = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div[2]/div[1]/div[2]/form/div[1]/span[2]/i"));
         Assert.assertEquals(true, iconAlert.isDisplayed());
@@ -73,21 +90,12 @@ public class RegistroDZoneTest {
         System.out.println("Se muestra el icono? "+iconAlert.isDisplayed());
         
         //Validar el mensaje del correo
-        WebElement emailErrorMessage = driver.findElement(By.xpath("//div[@data-validate=\"Please enter a valid email address\"]"));
+        WebElement 	emailErrorMessage = driver.findElement(By.xpath("//div[@data-validate=\"Please enter a valid email address\"]"));
         String attribute = emailErrorMessage.getAttribute("data-validate");
         System.out.println("Valor del attribute::: "+attribute);
         
         Assert.assertEquals("Please enter a valid email address", attribute);
         
-        //Validar el mensaje del username
-        iconAlert = driver.findElement(By.xpath("//*[@id=\"login\"]/div/div[2]/div[1]/div[2]/form/div[2]/span[2]/i"));
-        Assert.assertEquals(true, iconAlert.isDisplayed());
-        
-        WebElement usernameErrorMessage = driver.findElement(By.xpath("//div[@data-validate=\"Please enter a valid username\"]"));
-        attribute = usernameErrorMessage.getAttribute("data-validate");
-        System.out.println("Valor del attribute::: "+attribute);
-        
-        Assert.assertEquals("Please enter a valid username", attribute);
     }
     
     
@@ -97,6 +105,41 @@ public class RegistroDZoneTest {
         driver.quit();
     }
     
+    
+    
+    /*
+     
+     
+        
+        
+        
+        //Validar el mensaje del username
+        
+        WebElement usernameErrorMessage = driver.findElement(By.xpath("//div[@data-validate=\"Please enter a valid username\"]"));
+        attribute = usernameErrorMessage.getAttribute("data-validate");
+        System.out.println("Valor del attribute::: "+attribute);
+        
+        Assert.assertEquals("Please enter a valid username", attribute);
+        
+         
+        
+        //Validate el check del agreement
+        WebElement checkAgreement = driver.findElement(By.xpath("//*[@id=\"accept-agreement\"]"));
+        
+        checkAgreement.click();
+        
+        try{
+            TimeUnit.SECONDS.sleep(3);
+        }
+        catch(InterruptedException e){
+            e.printStackTrace();
+        }
+        
+        
+        //Adicionar codigo para llamar al API de la herramienta de administracion de casos de prueba
+
+     
+     * */
     
     
 }
